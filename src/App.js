14fefce4299408c29e4a6c291b5cf24cddf0258f
Hchan2 <seongchan_@naver.com>
2025-10-18 22:01:18 +0900

    feat(input): '구분자(커스텀)로 문자열을 분리합니다입력합니다.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,38 @@ class App {
     const userInput = await MissionUtils.Console.readLineAsync();
     await this.checkUserInput(userInput);
   }
+
+  async checkUserInput(userInput) {
+    const getSeparatedInput = await this.separateInput(userInput);
+    const validatedInput = await this.validateInputText(getSeparatedInput);
+    const sumNumbersValue = await this.gameResult(validatedInput);
+
+    MissionUtils.Console.print(`결과 : ${sumNumbersValue}`);
+  }
+
+  async separateInput(userInput) {
+    const getCheckCustomSeparator = await this.isCustomSeparator(userInput);
+
+    if (getCheckCustomSeparator) {
+      return await this.separateCustom(userInput);
+    }
+    return await this.separateNormal(userInput);
+  }
+
+  async separateNormal(userInput) {
+    return userInput.split(/,|:/);
+  }
+
+  async separateCustom(userInput) {
+    const customMatch = userInput.match(/^\/\/(.)\\n/);
+    const customDelimiter = customMatch[1];
+    const numbersString = userInput.substring(customMatch[0].length);
+    const delimiterRegex = new RegExp(`[${customDelimiter}|\\n]`);
+
+    return numbersString.split(delimiterRegex).filter(s => s !== '');
+  }
+
+
 }
 
 export default App;
