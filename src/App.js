3def070867334eb396ed7f4feb547fb7a3f4dc3c
Hchan2 <seongchan_@naver.com>
2025-10-18 21:56:54 +0900

    feat(user): '사용자 입력' 사용자가 임의의 문자열을 입력합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+  async run() {
+    await this.start();
+  }
+
+  async start() {
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+    await this.inputText();
+  }
+
+  async inputText() {
+    const userInput = await MissionUtils.Console.readLineAsync();
+    await this.checkUserInput(userInput);
+  }
 }
 
 export default App;
