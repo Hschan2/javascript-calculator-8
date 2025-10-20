698814c2e3f85c106bd26dbff43eb2f183366d11
Hchan2 <seongchan_@naver.com>
2025-10-20 19:09:59 +0900

    refactor(App): '분리된 클래스 함수 적용' 각 역할에 따라 분리된 클래스를 적용합니다.

--- src/App.js
+++ src/App.js
@@ -1,87 +1,33 @@
-import { MissionUtils } from '@woowacourse/mission-utils'
+import User from './User';
+import Valid from './Valid';
+import Game from './Game';
 
 class App {
   async run() {
-    await this.start();
+    await this.user();
   }
 
-  async start() {
-    const userInput = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-    await this.checkUserInput(userInput);
+  async user() {
+    const userInput = new User();
+    await userInput.initializeUserInput();
+
+    const separatedInput = userInput.getSeparatedInput();
+    await this.valid(separatedInput);
   }
 
-  async checkUserInput(userInput) {
-    const getSeparatedInput = await this.separateInput(userInput);
-    const validatedInput = await this.validateInputText(getSeparatedInput);
-    const sumNumbersValue = await this.gameResult(validatedInput);
+  async valid(refinedInput) {
+    const validation = new Valid(refinedInput);
+    await validation.isValid();
 
-    MissionUtils.Console.print(`결과 : ${sumNumbersValue}`);
+    const validInput = validation.getValidInput();
+    await this.game(validInput);
   }
 
-  async separateInput(userInput) {
-    const getCheckCustomSeparator = await this.isCustomSeparator(userInput);
+  async game(refinedInput) {
+    const setGame = new Game(refinedInput);
+    await setGame.isSum();
 
-    if (getCheckCustomSeparator) {
-      return await this.separateCustom(userInput);
-    }
-    return await this.separateNormal(userInput);
-  }
-
-  async isCustomSeparator(userInput) {
-    if (userInput.includes('//') && userInput.includes('\\n')) {
-      return true;
-    }
-
-    return false;
-  }
-
-  async separateNormal(userInput) {
-    return userInput.split(/,|:/);
-  }
-
-  async separateCustom(userInput) {
-    const customMatch = userInput.match(/^\/\/(.)\\n/);
-    const customDelimiter = customMatch[1];
-    const numbersString = userInput.substring(customMatch[0].length);
-    const delimiterRegex = new RegExp(`[${customDelimiter}|\\n]`);
-
-    return numbersString.split(delimiterRegex).filter(s => s !== '');
-  }
-
-  async validateInputText(userInput) {
-    const checkIncludedString = await this.isIncludedString(userInput);
-    const checkIncludedNegativeNumbers = await this.isIncludedNegativeNumber(checkIncludedString);
-
-    return checkIncludedNegativeNumbers;
-  }
-
-  async isIncludedString(userInput) {
-    const invalidChar = userInput.filter(v => isNaN(v));
-
-    if (invalidChar.length > 0) {
-      throw new Error("[ERROR] 문자열에 숫자가 아닌 문자가 포함되어 있습니다.");
-    }
-
-    return userInput;
-  }
-
-  async isIncludedNegativeNumber(userInput) {
-    const negativeNumbers = userInput.filter(v => Number(v) < 0);
-
-    if (negativeNumbers.length > 0) {
-      throw new Error("[ERROR] 문자열에 음수가 포함되어 있습니다.");
-    }
-
-    return userInput;
-  }
-
-  async sumNumbers(numberArr) {
-    return numberArr.reduce((acc, cur) => acc + Number(cur), 0);
-  }
-
-  async gameResult(userInput) {
-    const getSumNumbers = await this.sumNumbers(userInput);
-    return getSumNumbers;
+    await setGame.resultPrint();
   }
 }
 
