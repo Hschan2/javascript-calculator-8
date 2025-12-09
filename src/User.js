175517d50f801bf43f785e08fd379d8c5de853eb
Hchan2 <seongchan_@naver.com>
2025-12-09 16:02:31 +0900

    refactor: use a userInput filter and remove a get method

--- src/User.js
+++ src/User.js
@@ -1,16 +1,12 @@
 import { MissionUtils } from '@woowacourse/mission-utils'
+import { INPUT_MESSAGE } from './constants/message';
 
 class User {
     constructor() {
-        this.separatedInput = null;
     }
 
-    async initializeUserInput() {
-        const userInput = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-        this.separatedInput = await this.isIncludedCustom(userInput);
-    }
-
-    async isIncludedCustom(userInput) {
+    async promptAndParse() {
+        const userInput = await MissionUtils.Console.readLineAsync(INPUT_MESSAGE.USER_INPUT);
         if (userInput.includes('//') && userInput.includes('\\n')) {
             return this.isCustomSeparator(userInput);
         }
@@ -18,7 +14,15 @@ class User {
         return this.isNotCustomSeparator(userInput);
     }
 
-    async isCustomSeparator(userInput) {
+    isIncludedCustom(userInput) {
+        if (userInput.includes('//') && userInput.includes('\\n')) {
+            return this.isCustomSeparator(userInput);
+        }
+
+        return this.isNotCustomSeparator(userInput);
+    }
+
+    isCustomSeparator(userInput) {
         const customMatch = userInput.match(/^\/\/(.)\\n/);
         const customDelimiter = customMatch[1];
         const numbersString = userInput.substring(customMatch[0].length);
@@ -27,12 +31,8 @@ class User {
         return numbersString.split(delimiterRegex).filter(s => s !== '');
     }
 
-    async isNotCustomSeparator(userInput) {
-        return userInput.split(/,|:/);
-    }
-
-    getSeparatedInput() {
-        return this.separatedInput;
+    isNotCustomSeparator(userInput) {
+        return userInput.split(/,|:/).filter(s => s !== '');
     }
 }
 
